0ec3d4bf45d60762a3f07b2a9c0ede2c1b62312f
tetblue <40160865+tetblue@users.noreply.github.com>
2023-02-06 14:21:07 +0800

    Create 11_container_with_most_water.py

---
+++ 11_container_with_most_water.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right, area, min_height = 0, len(height) - 1, 0, 0
+        while left < r:
+            if height[left] > min_height and height[right] > min_height:
+                min_height=min(height[left], height[right])
+                area = max(area, (right - l) * min_height)
+            if height[left] < height[right]:
+                left+=1
+                while height[left]<min_height:
+                    left+=1
+            else:
+                right-=1
+                while height[right]<min_height:
+                    right-=1			
+        return area
